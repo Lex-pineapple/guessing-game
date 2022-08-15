e6fcde096261cdf512923109a1bef516d9b1f7bb
Lex <pineapplepear55@gmail.com>
2022-08-15 23:15:14 +0300

    feat: add main functional

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+    
     guess() {
-
+        this.pivot = Math.round((this.max+this.min)/2);
+        return this.pivot;
     }
 
     lower() {
-
+        this.max = this.pivot;
+        console.log(this.max);
     }
 
     greater() {
-
+        this.min = this.pivot;
+        console.log(this.min);
     }
 }
 
 module.exports = GuessingGame;
+
+
